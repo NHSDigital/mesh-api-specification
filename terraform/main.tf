526df94abdd078e8e9d9306465b876a1cc177b5d
jalal <jalalhosseiny@gmail.com>
2020-04-24 11:09:47 +0100

    AMB-35 Fix pullrequest build in azure-pipelines.yml and add auth flow

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,5 +19,7 @@ module "hello-world" {
   apigee_environment = var.apigee_environment
     proxy_type         = length(regexall("sandbox", var.apigee_environment)) > 0 ? "sandbox" : "live"
   namespace          = var.namespace
-  make_api_product   = true
+  make_api_product   = length(var.namespace) == 0
+  api_product_display_name = "Hello World Api"
+  api_product_description  = "TODO: Link to docs when they are published"
 }
