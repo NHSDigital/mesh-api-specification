c19e608d271f0ed35cddb0413d5d0cbc64fd6c1d
jalal <jalalhosseiny@gmail.com>
2020-04-24 12:00:35 +0100

    AMB-35 use only sandbox

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ module "hello-world" {
   name               = "hello-world"
   path               = "hello-world"
   apigee_environment = var.apigee_environment
-    proxy_type         = length(regexall("sandbox", var.apigee_environment)) > 0 ? "sandbox" : "live"
+  proxy_type         = "sandbox"
   namespace          = var.namespace
   make_api_product   = length(var.namespace) == 0
   api_product_display_name = "Hello World Api"
