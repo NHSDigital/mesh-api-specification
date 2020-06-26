2490209c9b3bb4970a7e05ea9505db15d3ee6187
Ryan  Thomas <ryan.thomas@airelogic.com>
2020-06-26 15:42:11 +0100

    AMB-109 revert back to apimocker

--- docker/hello-world-sandbox/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const getStatus = require('./get-status')
-const helloWorld = require('./hello-world')
-const helloApplication = require('./hello-application')
-const helloUser = require('./hello-user')
-
-const routes = [].concat(getStatus, helloWorld, helloApplication, helloUser)
-
-module.exports = routes
