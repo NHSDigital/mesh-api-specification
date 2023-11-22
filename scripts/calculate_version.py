fa9240cfcee46bb3628e52f0ea5b047dd829dc1a
matt-mercer <449892+matt-mercer@users.noreply.github.com>
2023-11-22 13:41:06 +0000

    mesh-1761: switch from pairwise back to previous implementation

--- scripts/calculate_version.py
+++ scripts/calculate_version.py
@@ -18,7 +18,6 @@ Commands:
 
 import itertools
 import os.path
-from itertools import pairwise
 
 import git
 import semver
@@ -54,7 +53,7 @@ def is_minor_inc(commit):
 
 def without_empty(commits):
     """Takes a list of commits and returns a list without empty commits"""
-    pairs = pairwise(commits)
+    pairs = zip(commits, commits[1:], strict=False)  # noqa: RUF007
 
     for fst, snd in pairs:
         if fst.tree != snd.tree:
